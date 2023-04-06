5c420bfb73167010242fe45aabf6daa5454525f3
Juan <juampi.parzybal@gmail.com>
2023-04-06 19:22:05 -0300

    ejercicio 6

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,18 @@
+import React, { useState } from "react";
+
+
+
 export default function SearchBar({onSearch}) {
+
+   const [id, setId] = useState("");
+
+   const handleChange = (event) => {
+      setId(event.target.value)
+   }
+
    return (
       <div>
-        <input type='search' />
+        <input value ={id} type='search' onChange = {handleChange} />
          <button onClick={onSearch}>Agregar</button>
       </div>
    );
